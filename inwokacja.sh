c3ae6dcc99240cc26128984e2347b90a96884886
B269905-2024 <s2713107@ed.ac.uk>
2024-10-08 23:57:06 +0100

    second part of the poem added

--- inwokacja.sh
+++ inwokacja.sh
@@ -7,4 +7,10 @@
 
 #Widzę i opisuję, bo tęsknię po tobie.
 
+#Panno Święta, co Jasnej bronisz Częstochowy
 
+#I w Ostrej świecisz Bramie! Ty, co gród zamkowy
+
+#Nowogródzki ochraniasz z jego wiernym ludem!
+
+#Jak mnie dziecko do zdrowia powróciłaś cudem
